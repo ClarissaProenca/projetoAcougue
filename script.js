c9becee8ce95f06f73e7fc6f62bad03d7cdc985c
ClarissaProenca <114036201+ClarissaProenca@users.noreply.github.com>
2023-06-06 11:34:37 +0000

    Adicionando efeito nas sessões de carne assim que o usuário der scrool down da página

--- script.js
+++ script.js
@@ -65,6 +65,24 @@ function closeAllSelect(elmnt) {
 }
 document.addEventListener("click", closeAllSelect);
 
+function reveal() {
+  var reveals = document.querySelectorAll(".reveal");
+
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 120;
+
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
+  }
+}
+
+window.addEventListener("scroll", reveal);
+
 // acordeaum
 const itens = document.querySelectorAll('.accordion-item');
 
