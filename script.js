01889736bf0967e68fc6353e161f0fe8b25726a5
ClarissaProenca <114036201+ClarissaProenca@users.noreply.github.com>
2023-06-22 12:42:54 +0000

    terminando o trabalho

--- script.js
+++ script.js
@@ -1,118 +1,150 @@
-// selecionar tipo de carne favorita
-var x, i, j, l, ll, selElmnt, a, b, c;
-x = document.getElementsByClassName("custom-select");
-l = x.length;
-for (i = 0; i < l; i++) {
-  selElmnt = x[i].getElementsByTagName("select")[0];
-  ll = selElmnt.length;
-  a = document.createElement("DIV");
-  a.setAttribute("class", "select-selected");
-  a.innerHTML = selElmnt.options[selElmnt.selectedIndex].innerHTML;
-  x[i].appendChild(a);
-  b = document.createElement("DIV");
-  b.setAttribute("class", "select-items select-hide");
-  for (j = 1; j < ll; j++) {
-    c = document.createElement("DIV");
-    c.innerHTML = selElmnt.options[j].innerHTML;
-    c.addEventListener("click", function(e) {
-      var y, i, k, s, h, sl, yl;
-      s = this.parentNode.parentNode.getElementsByTagName("select")[0];
-      sl = s.length;
-      h = this.parentNode.previousSibling;
-      for (i = 0; i < sl; i++) {
-        if (s.options[i].innerHTML == this.innerHTML) {
-          s.selectedIndex = i;
-          h.innerHTML = this.innerHTML;
-          y = this.parentNode.getElementsByClassName("same-as-selected");
-          yl = y.length;
-          for (k = 0; k < yl; k++) {
-            y[k].removeAttribute("class");
-          }
-          this.setAttribute("class", "same-as-selected");
-          break;
-        }
-      }
-      h.click();
-    });
-    b.appendChild(c);
-  }
-  x[i].appendChild(b);
-  a.addEventListener("click", function(e) {
-    e.stopPropagation();
-    closeAllSelect(this);
-    this.nextSibling.classList.toggle("select-hide");
-    this.classList.toggle("select-arrow-active");
-  });
-}
-function closeAllSelect(elmnt) {
-  var x, y, i, xl, yl, arrNo = [];
-  x = document.getElementsByClassName("select-items");
-  y = document.getElementsByClassName("select-selected");
-  xl = x.length;
-  yl = y.length;
-  for (i = 0; i < yl; i++) {
-    if (elmnt == y[i]) {
-      arrNo.push(i)
-    } else {
-      y[i].classList.remove("select-arrow-active");
-    }
-  }
-  for (i = 0; i < xl; i++) {
-    if (arrNo.indexOf(i)) {
-      x[i].classList.add("select-hide");
-    }
-  }
-}
-document.addEventListener("click", closeAllSelect);
-
-function reveal() {
-  var reveals = document.querySelectorAll(".reveal");
-
-  for (var i = 0; i < reveals.length; i++) {
-    var windowHeight = window.innerHeight;
-    var elementTop = reveals[i].getBoundingClientRect().top;
-    var elementVisible = 120;
-
-    if (elementTop < windowHeight - elementVisible) {
-      reveals[i].classList.add("active");
-    } else {
-      reveals[i].classList.remove("active");
-    }
-  }
-}
-
-window.addEventListener("scroll", reveal);
-
 // acordeaum
 const itens = document.querySelectorAll('.accordion-item');
 
 for (const item of itens) {
-
   const header = item.querySelector('.accordion-header');
-
   const conteudo = item.querySelector('.conteudo');
-
+  
   header.addEventListener('click', function() {
-
     if (item.classList.contains('active')) {
-
       conteudo.style.maxHeight = null;
       item.classList.remove('active');
     }
     else {
-
       for (const outroItem of itens) {
         if (outroItem.classList.contains('active')) {
-
           const outroConteudo = outroItem.querySelector('.conteudo');
-
           outroConteudo.style.maxHeight = null;
           outroItem.classList.remove('active');
         }
       }
-
       conteudo.style.maxHeight = conteudo.scrollHeight + 'px';
       item.classList.add('active');
     }
   });
+}
+
+// formulario
+
+// preferencias
+var el = document.querySelectorAll('.container');
+for (let i = 0; i < el.length; i++) {
+  el[i].onclick = function() {
+    var c = 0;
+    while (c < el.length) {
+      el[c++].className = 'container';
+    }
+    el[i].className = 'Selecionadas container';
+  };
+}
+
+// FAVORITA  
+const botao = document.querySelector(".dropbtn");
+const opcoesCarne = document.querySelectorAll(".dropdown-content p");
+let opcaoFoiSelecionada = false;
+
+opcoesCarne.forEach(function(opcao) {
+  opcao.addEventListener('click', function() {
+    const valorOpcao = this.getAttribute("value");
+    botao.textContent = valorOpcao;
+    opcaoFoiSelecionada = true;
+  });
+});
+
+
+// comunicações
+
+let check = document.querySelectorAll(".comunicacoes");
+let container = document.querySelectorAll(".checkDefault");
+let check1 = check[2];
+let check2 = check[1];
+let checkTudo = check[0];
+
+checkTudo.addEventListener("click", function(){
+  if(checkTudo.classList.value == "comunicacoes intermed"){
+    check1.setAttribute("class", "comunicacoes");
+    check2.setAttribute("class", "comunicacoes");
+    checkTudo.setAttribute("class", "comunicacoes");
+  } else {
+    checkTudo.classList.toggle("iconActive");
+    checkTudo.classList.remove("intermed");
+    check1.classList.toggle("iconActive");
+    check2.classList.toggle("iconActive");
+  }
+});
+
+check1.addEventListener("click", function(){
+  check1.classList.toggle("iconActive");
+  checkTudo.classList.toggle("intermed");
+  if(check2.classList.contains("iconActive")){
+    checkTudo.classList.add("iconActive");
+  } else {
+    checkTudo.classList.remove("iconActive");
+  }
+});
+
+check2.addEventListener("click", function(){
+  check2.classList.toggle("iconActive");
+  checkTudo.classList.toggle("intermed");
+  if(check1.classList.contains("iconActive")){
+    checkTudo.classList.add("iconActive");
+  } else {
+    checkTudo.classList.remove("iconActive");
+  }
+});
+
+// enviar
+function enviar() {
+  event.preventDefault();
+  let nome = document.getElementById("nome").value;
+  let email = document.getElementById("email").value;
+  let telefone = document.getElementById("telefone").value;
+  if (nome === "" || email === "" || telefone === "") {
+    alert("preencha os campos obrigatórios");
+  } else if (!nome.match(/^[a-z]{2,}( [a-z]+)*?( [a-z]{2,}){1,}$/i)) {
+    alert("Inclua nome e sobrenome");
+  } else if (!email.match(/^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/)) {
+    alert("Inclua email válido");
+  } else if(!verificaTelefone()){
+    alert("Informar telefone válido");
+  } else if(!preferenciaTaMarcada()){
+    alert("Selecionar pelo menos uma carne de preferência");
+  } else if (!opcaoFoiSelecionada) {
+    alert("Selecionar o tipo de carne favorita");
+  } else if (!verificarTextarea()){
+    alert("Mensagem deve conter pelo menos 5 caracteres");
+  }
+  else {
+    alert("Formulário enviado")
+  }
+
+  function verificaTelefone(){
+    let telefoneVerificado = telefone.replace(/[\(\)\-\s]/g, "");
+    if(telefoneVerificado.length === 11){
+      return true
+    } else {
+      return false;
+    }
+  }
+
+  function preferenciaTaMarcada(){
+    for (let i = 0; i < el.length; i++) {
+      if(el[i].classList.contains("Selecionadas")){
+        return true;
+      } else {
+        return false;
+      }
+    }
+  }
+
+  function verificarTextarea() {
+  var textarea = document.querySelector("textarea");
+  var texto = textarea.value;
+
+  if (texto.length < 5 || texto ==="") {
+    return false;
+  } else {
+    return true;
+  }
+}
 }
